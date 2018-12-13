92f5b7d0f6db7a010f67f42b4bd73aad4f4c22d5
unknown <DAM216@acarballeira.local>
2018-12-13 11:03:13 +0100

    hecho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NuevaPage } from '../pages/nueva/nueva';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NuevaPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NuevaPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NuevaPage } from '../nueva/nueva'
+import { ToastController } from 'ionic-angular';
+import { LoadingController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  num : number=0;
+  
+ 
+  incrementar(){
+    this.num++;
+  }
+  nuevapag(){
+    this.navCtrl.push(NuevaPage);
+  }
+  mensaje(){
+      let toast = this.toastCtrl.create({
+        message: 'Aida',
+        duration: 3000,
+        position: 'top'
+      });
+    
+      toast.onDidDismiss(() => {
+        console.log('Dismissed toast');
+      });
+    
+      toast.present();
+    
+  }
+  mensaje2(){
+    if(this.num%2==0){
+      let loading = this.loadingCtrl.create({
+        content: 'cargando'
+      });
+      loading.present();
+    
+      setTimeout(() => {
+        loading.dismiss();
+      }, 2000);
+      this.navCtrl.push(NuevaPage);
+    }
+  }
+  constructor(private toastCtrl: ToastController , public loadingCtrl: LoadingController , public navCtrl: NavController) {
 
   }
 
